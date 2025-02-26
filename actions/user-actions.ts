580567fc3162672881ba9b548757522e1e70ee5e
Avishek Majumder <avishekalex15@gmail.com>
2025-02-26 18:57:32 +0800

    Add logging for filter parameters in getUsers function -> Later to be used for actually filtering the data

--- actions/user-actions.ts
+++ actions/user-actions.ts
@@ -4,6 +4,13 @@ import { Users } from "@/db/schema";
 export async function getUsers(filter_params: {
     [key: string]: string | string[] | undefined;
 }) {
-    console.log(filter_params);
+    // Log key-value pairs if filter_params exists
+    if (Object.keys(filter_params).length > 0) {
+        console.log("Filter parameters:");
+        for (const [key, value] of Object.entries(filter_params)) {
+            console.log(`${key}: ${JSON.stringify(value)}`);
+        }
+    }
+
     return await db.select().from(Users);
 }
