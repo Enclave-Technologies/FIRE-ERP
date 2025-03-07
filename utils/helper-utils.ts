1ee12f68a5d4b1ff4b5e58e6b8ac74f6d5919cd1
Avishek Majumder <avishekalex15@gmail.com>
2025-03-07 12:40:27 +0800

    Refactor chunkArray function to use generics for improved type safety

--- utils/helper-utils.ts
+++ utils/helper-utils.ts
@@ -1,4 +1,4 @@
-export function chunkArray(array: any[], chunkSize: number) {
+export function chunkArray<T>(array: T[], chunkSize: number): T[][] {
     const chunks = [];
     for (let i = 0; i < array.length; i += chunkSize) {
         chunks.push(array.slice(i, i + chunkSize));
